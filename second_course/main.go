96c1e34ee0597a00c72c9dd96dcb1ad7cab95838
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-14 18:50:26 +0200

    const can be overrided when declared at package lever inside main function

--- second_course/main.go
+++ second_course/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var r bool
-	fmt.Printf("%v %T\n", r, r)
+	var r = complex(3, 2)
+	fmt.Printf("%v %T\n", real(r), r)
+	fmt.Printf("%v %T", imag(r), r)
 }
