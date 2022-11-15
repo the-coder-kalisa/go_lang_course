57e9d82a830c52492913066e0646ec67128fbb95
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-15 09:29:14 +0200

    switch on interfaces type

--- second_course/main.go
+++ second_course/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	i := 10
-	switch {
-	case i <= 10:
-		fmt.Println("less than or equal 10")
-		fallthrough
-	case i <= 20:
-		fmt.Println("less than or equal 20")
+	var i interface{} = 4
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an int")
+		break
+		fmt.Println("this will pring too")
+	case float64:
+		fmt.Println("i is a float64")
+	case string:
+		fmt.Println("i is a string")
+	case [3]int:
+		fmt.Println("i is [3]int")
 	default:
-		fmt.Println("greater than 2-")
+		fmt.Println("i is another type")
 	}
-}	
+}
