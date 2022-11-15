54e61069d7e9d8cafba9bf9a0c0a9ace372d6d86
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-15 09:41:48 +0200

    loops and looping inside an arrray, maps and slices

--- second_course/main.go
+++ second_course/main.go
@@ -1,21 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var i interface{} = 4
-	switch i.(type) {
-	case int:
-		fmt.Println("i is an int")
-		break
-		fmt.Println("this will pring too")
-	case float64:
-		fmt.Println("i is a float64")
-	case string:
-		fmt.Println("i is a string")
-	case [3]int:
-		fmt.Println("i is [3]int")
-	default:
-		fmt.Println("i is another type")
+	s := [3]int{1, 2, 4}
+	for k, v := range s{
+		fmt.Println(k, v)
 	}
 }
