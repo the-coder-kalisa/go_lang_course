f6cf31f45e0fd6f0e8935dc053e154d099bb1f57
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-15 09:18:10 +0200

    expert in switch cases

--- second_course/main.go
+++ second_course/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	switch i := 2 + 3; i {
-	case 1, 5, 10:
-		fmt.Println("one")
-	case 2, 4, 6:
-		fmt.Println("two")
+	i := 10
+	switch {
+	case i <= 10:
+		fmt.Println("less than or equal 10")
+		fallthrough
+	case i <= 20:
+		fmt.Println("less than or equal 20")
 	default:
-		fmt.Println("not one or two")
+		fmt.Println("greater than 2-")
 	}
-}
+}	
